49dff4d298f8cbc4724df0784ffcfa43ca045bbe
Makinde1034 <makinde1034@gmail.com>
2022-08-16 15:53:08 +0100

    feat: add toast notification

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useCartegoryState } from '../state/categories/hooks'
